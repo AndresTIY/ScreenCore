4076a7b0d0a82b1053480baa87e4f0e1f289b03f
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-05 16:04:02 -0500

    button toggle works for each individual topic

--- app/scripts/components/m_topic.js
+++ app/scripts/components/m_topic.js
@@ -6,23 +6,12 @@ import DetailedView from "./m_detailed_view.js";
 class MovieTopic extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { topicExpand: false };
-    this.handleTopicExpand = this.handleTopicExpand.bind(this);
-  }
-
-  handleTopicExpand() {
-    this.setState({
-      topicExpand: !this.state.topicExpand
-    });
   }
 
   render() {
     let topics = this.props.topics;
     let movieId = Number(this.props.movieId);
-    let classToggle = "hide";
-    if (this.state.topicExpand) {
-      classToggle = "show";
-    }
+
     if (topics !== null) {
       return (
         <ul>
@@ -32,27 +21,12 @@ class MovieTopic extends React.Component {
               let convert = moment(time).format("h:mma MM/DD/YYYY");
               return (
                 <div key={topic.objectId}>
-                  <li>
-                    <TopicExpand onChange={this.handleTopicExpand} />
-
-                    {topic.topic}
-                    {" "}
-                    created by
-                    {" "}
-                    {topic.username}
-                    {" "}
-                    on
-                    {" "}
-                    {convert}
-                    {" "}
-
-                  </li>
-                  <li>
-                    <div className={classToggle}>
-                      <DetailedView message={topic.message} />
-                    </div>
-
-                  </li>
+                  <TopicExpand
+                    handleTopicExpand={this.handleTopicExpand}
+                    topics={topic}
+                    time={convert}
+                    id={topic.objectId}
+                  />
                 </div>
               );
             }
@@ -66,3 +40,75 @@ class MovieTopic extends React.Component {
 } //end of movie topic
 
 export default MovieTopic;
+////////////////////////////////////////////////////////////////
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import TopicExpand from "./m_topic_expand.js";
+// import DetailedView from "./m_detailed_view.js";
+//
+// class MovieTopic extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { topicExpand: false };
+//     this.handleTopicExpand = this.handleTopicExpand.bind(this);
+//   }
+//
+//   handleTopicExpand() {
+//     this.setState({
+//       topicExpand: !this.state.topicExpand
+//     });
+//   }
+//
+//   render() {
+//     let topics = this.props.topics;
+//     let movieId = Number(this.props.movieId);
+//     let classToggle = "hide";
+//     if (this.state.topicExpand) {
+//       classToggle = "show";
+//     }
+//     if (topics !== null) {
+//       return (
+//         <ul>
+//           {this.props.topics.map((topic, i) => {
+//             if (topic.movie_id === movieId) {
+//               let time = topic.created;
+//               let convert = moment(time).format("h:mma MM/DD/YYYY");
+//               return (
+//                 <div key={topic.objectId}>
+//                   <li>
+//                     <TopicExpand handleTopicExpand={this.handleTopicExpand} />
+//
+//                     {topic.topic}
+//                     {" "}
+//                     created by
+//                     {" "}
+//                     {topic.username}
+//                     {" "}
+//                     on
+//                     {" "}
+//                     {convert}
+//                     {" "}
+//
+//                   </li>
+//                   <li>
+//                     <div className={classToggle}>
+//                       <DetailedView
+//                         objectId={topic.objectId}
+//                         message={topic.message}
+//                       />
+//                     </div>
+//
+//                   </li>
+//                 </div>
+//               );
+//             }
+//           })}
+//         </ul>
+//       );
+//     } else {
+//       return <p>Loading...</p>;
+//     }
+//   }
+// } //end of movie topic
+//
+// export default MovieTopic;
--- app/scripts/components/m_topic_expand.js
+++ app/scripts/components/m_topic_expand.js
@@ -1,11 +1,51 @@
 import React from "react";
+import DetailedView from "./m_detailed_view.js";
 
 class TopicExpand extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { topicExpand: false };
+    this.handleTopicExpand = this.handleTopicExpand.bind(this);
+  }
+
+  handleTopicExpand() {
+    this.setState({
+      topicExpand: !this.state.topicExpand
+    });
   }
   render() {
-    return <button onClick={this.props.onChange} className="btn">+</button>;
+    let topics = this.props.topics;
+    let classToggle = "hide";
+    if (this.state.topicExpand) {
+      classToggle = "show";
+    }
+    return (
+      <div>
+        <li>
+          <button onClick={this.handleTopicExpand} className="btn">
+            +
+          </button>
+
+          {topics.topic}
+          {" "}
+          created by
+          {" "}
+          {topics.username}
+          {" "}
+          on
+          {" "}
+          {this.props.time}
+          {" "}
+
+        </li>
+        <li>
+          <div className={classToggle}>
+            <DetailedView message={topics.message} />
+          </div>
+
+        </li>
+      </div>
+    );
   }
 }
 export default TopicExpand;
