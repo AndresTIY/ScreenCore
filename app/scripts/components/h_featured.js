65db86a39000b333d60935e2f109694928745b8a
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-08 14:07:05 -0500

    text format on featured card adjusted

--- app/scripts/components/h_featured.js
+++ app/scripts/components/h_featured.js
@@ -19,15 +19,14 @@ class FeaturedMovie extends React.Component {
       return (
         <div className="row featured-card">
           <div className="featured-backdrop">
-            <div className="overlay">
-              <img
-                className="backdrop-img"
-                src={`${urlBack}${mostPopular.backdrop_path}`}
-              />
-            </div>
+
+            <img
+              className="backdrop-img"
+              src={`${urlBack}${mostPopular.backdrop_path}`}
+            />
+
           </div>
           <div className="col s12 m8 l8">
-
             <div className="card medium horizontal">
 
               <div className="card-image">
@@ -38,15 +37,30 @@ class FeaturedMovie extends React.Component {
               </div>
               <div className="card-stacked">
                 <div className="card-content ">
-                  <p>{mostPopular.title}</p>
-                  <p>
-                    Release Date:
+                  <h6 className="header">
+                    MOST POPULAR NEW RELEASE
+                  </h6>
+
+                  <h4 className="movie-title">
+                    {mostPopular.title.toUpperCase()}
+                  </h4>
+                  <p className="release-date">
+                    RELEASE DATE:
                     {" "}
                     {moment(mostPopular.release_date).format("M/DD/YYYY")}
                   </p>
+                  <div className="review-section">
 
-                  <p>Review Average: {mostPopular.vote_average}</p>
-                  <p>Description: {mostPopular.overview}</p>
+                    <span className="vote-avg">
+                      {mostPopular.vote_average} |
+                    </span>
+                    <div className="vote-text">
+                      <span className="avg"> AVERAGE </span><br />
+                      <span className="rating"> RATING</span>
+                    </div>
+
+                  </div>
+                  <p>{mostPopular.overview}</p>
                 </div>
                 <div className="card-action">
                   <Link to={"/discussion/" + mostPopular.id}>
