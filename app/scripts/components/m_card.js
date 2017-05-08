9c0b29c33312ffd1c82821fa73dabd16442d0514
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-08 14:36:39 -0500

    all movie cards have same style

--- app/scripts/components/m_card.js
+++ app/scripts/components/m_card.js
@@ -13,29 +13,37 @@ class MovieDiscussCard extends React.Component {
     return (
       <div className="row featured-card">
 
+        <div className="featured-backdrop">
+          <img
+            className="backdrop-img"
+            src={`${urlBack}${movie.backdrop_path}`}
+          />
+        </div>
+
         <div className="col s12 m8 l8">
-          <div className="card horizontal">
+          <div className="card medium horizontal">
             <div className="card-image">
               <img src={urlPath + movie.poster_path} />
             </div>
             <div className="card-stacked">
               <div className="card-content ">
-                <p>{movie.title}</p>
-                <p>
-                  Release Date: {moment(movie.release_date).format("M/DD/YYYY")}
+                <h4 className="movie-title">{movie.title.toUpperCase()}</h4>
+                <p className="release-date">
+                  RELEASE DATE: {moment(movie.release_date).format("M/DD/YYYY")}
                 </p>
-                <p>Review Average: {movie.vote_average}</p>
-                <p>Description: {movie.overview}</p>
+                <div className="review-section">
+                  <span className="vote-avg">{movie.vote_average} |</span>
+                  <div className="vote-text">
+                    <span className="avg"> AVERAGE </span><br />
+                    <span className="rating"> RATING</span>
+                  </div>
+                </div>
+                <p>{movie.overview}</p>
               </div>
 
             </div>
           </div>
-          <div className="backdrop-poster">
-            <img
-              className="backdrop-img"
-              src={`${urlBack}${movie.backdrop_path}`}
-            />
-          </div>
+
         </div>
       </div>
     );
