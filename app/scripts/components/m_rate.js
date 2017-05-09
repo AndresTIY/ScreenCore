cb708b0b62c4a4a84ca81b953f9b461b36a78e74
Andres Cifuentes <aacifuentes@gmail.com>
2017-05-09 11:46:17 -0500

    stars showing up and styled to fit

--- app/scripts/components/m_rate.js
+++ app/scripts/components/m_rate.js
@@ -1,22 +1,26 @@
 import React from "react";
+import ReactStars from "react-stars";
+
+const ratingChanged = newRating => {
+  console.log(newRating);
+};
 
 class Rate extends React.Component {
   constructor(props) {
     super(props);
-    this.setState({ rating: newRating });
   }
 
   render() {
     return (
       <div className="rate">
         <p>Have you seen this movie? Give it a rating!</p>
-        <div className="icon-scale">
-          <i className="star material-icons">star</i>
-          <i className="star material-icons">star</i>
-          <i className="star material-icons">star</i>
-          <i className="star material-icons">star</i>
-          <i className="star material-icons">star</i>
-
+        <div className="stars">
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={24}
+            color2={"#ffd700"}
+          />
         </div>
       </div>
     );
